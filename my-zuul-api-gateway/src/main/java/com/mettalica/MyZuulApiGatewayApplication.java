dc347c00c7cd96b1cca6940176777f720426e949
shashank jain <shashank.jain43@gmail.com>
2019-09-08 13:57:04 +0530

    Implemented Zuul API Gateway

---
+++ my-zuul-api-gateway/src/main/java/com/mettalica/MyZuulApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.mettalica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyZuulApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyZuulApiGatewayApplication.class, args);
+	}
+
+}
